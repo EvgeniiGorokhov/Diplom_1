8b071452bbb8f514b57aabec47adff9f13d9f978
Evgenii Gorokhov <enigorohov@yandex.kz>
2024-10-19 14:59:29 +0500

    Diplom_1_unit_test

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,21 +1,41 @@
+import pytest
 from data import TestDataBase
 from conftest import data_base
-import pytest
 
 
 class TestDB:
     @pytest.mark.parametrize('index_bun, bun_name, bun_price', TestDataBase.TEST_BUN_FROM_DATABASE)
     def test_available_buns_db_success(self, data_base, index_bun, bun_name, bun_price):
+        """
+        Тест проверяет, что булочки из базы данных соответствуют ожидаемым значениям.
+        """
         data_buns = data_base.available_buns()
-        assert data_buns[index_bun].get_name() == bun_name and data_buns[index_bun].get_price() == bun_price
 
-    @pytest.mark.parametrize('index_i, type_ingredient, name_ingredient, price_ingredient',
+        # Проверка имени булочки
+        assert data_buns[
+                   index_bun].get_name() == bun_name, f"Ожидалось имя булочки {bun_name}, но получено {data_buns[index_bun].get_name()}"
+
+        # Проверка цены булочки
+        assert data_buns[
+                   index_bun].get_price() == bun_price, f"Ожидалась цена {bun_price}, но получено {data_buns[index_bun].get_price()}"
+
+    @pytest.mark.parametrize('ingredient_index, type_ingredient, name_ingredient, price_ingredient',
                              TestDataBase.TEST_DATABASE_INGREDIENTS)
-    def test_available_ingredients_db_success(self, data_base, index_i, type_ingredient, name_ingredient, price_ingredient):
+    def test_available_ingredients_db_success(self, data_base, ingredient_index, type_ingredient, name_ingredient,
+                                              price_ingredient):
+        """
+        Тест проверяет, что ингредиенты из базы данных соответствуют ожидаемым значениям.
+        """
         data_ingredients = data_base.available_ingredients()
-        assert data_ingredients[index_i].get_name() == name_ingredient
-        assert data_ingredients[index_i].get_type() == type_ingredient
-        assert data_ingredients[index_i].get_price() == price_ingredient
 
+        # Проверка имени ингредиента
+        assert data_ingredients[
+                   ingredient_index].get_name() == name_ingredient, f"Ожидалось имя ингредиента {name_ingredient}, но получено {data_ingredients[ingredient_index].get_name()}"
 
+        # Проверка типа ингредиента
+        assert data_ingredients[
+                   ingredient_index].get_type() == type_ingredient, f"Ожидался тип {type_ingredient}, но получено {data_ingredients[ingredient_index].get_type()}"
 
+        # Проверка цены ингредиента
+        assert data_ingredients[
+                   ingredient_index].get_price() == price_ingredient, f"Ожидалась цена {price_ingredient}, но получено {data_ingredients[ingredient_index].get_price()}"
